ca5a1d628fa9785bb6bf7cddc3a4f3bbdd11f35c
Marco Müllner <muellnermarco@gmail.com>
2021-08-17 16:02:17 +0200

    Added some common code

---
+++ notebooks/common/__init__.py
@@ -0,0 +1,1 @@
+from common.setup import *
---
+++ notebooks/common/brite_paths.py
@@ -0,0 +1,9 @@
+def load(field : int = None):
+    if(field is None):
+        field = input("Please enter a field: ")
+        try:
+            field = int(field)
+        except Exception as e:
+            print("Please enter a valid number")
+            raise AttributeError("Please enter a valid number for a field!")
+
